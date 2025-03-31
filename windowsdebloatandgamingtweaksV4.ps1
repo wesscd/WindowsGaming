673199b253a68e76be082542f62e5adeb69f3e3f
Cesar Marques <wesscd@gmail.com>
2025-03-31 10:45:36 -0300

    Update windowsdebloatandgamingtweaksV4.ps1

--- windowsdebloatandgamingtweaksV4.ps1
+++ windowsdebloatandgamingtweaksV4.ps1
@@ -127,7 +127,7 @@ function Show-Intro {
     "   ██║   ██╔══╝  ██║     ██╔══██║    ██╔══██╗██╔══╝  ██║╚██╔╝██║██║   ██║   ██║   ██╔══╝  ",
     "   ██║   ███████╗╚██████╗██║  ██║    ██║  ██║███████╗██║ ╚═╝ ██║╚██████╔╝   ██║   ███████╗",
     "   ╚═╝   ╚══════╝ ╚═════╝╚═╝  ╚═╝    ╚═╝  ╚═╝╚══════╝╚═╝     ╚═╝ ╚═════╝    ╚═╝   ╚══════╝",
-    "                                                                                  V0.7.2.0_",
+    "                                                                                  V0.7.2.0",
     "", "Bem-vindo ao TechRemote Ultimate Windows Debloater Gaming",
     "Este script otimizará o desempenho do seu sistema Windows.",
     "Um ponto de restauração será criado antes de prosseguir.",
