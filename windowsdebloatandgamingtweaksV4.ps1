0b9ee9b6181fb22a4c0d32ef1d4f50c2c342cb8c
Cesar Marques <wesscd@gmail.com>
2025-04-02 15:16:38 -0300

    Update windowsdebloatandgamingtweaksV4.ps1

--- windowsdebloatandgamingtweaksV4.ps1
+++ windowsdebloatandgamingtweaksV4.ps1
@@ -323,12 +323,16 @@ function Show-Intro {
     "Este script otimizará o desempenho do seu sistema Windows.",
     "Um ponto de restauração será criado antes de prosseguir.",
     "DESATIVE SEU ANTIVÍRUS e PRESSIONE QUALQUER TECLA para continuar!",
-    "", "", "=== Informações do Computador ===",
-    "Nome do Host: " + [System.Environment]::MachineName,
-    "Sistema Operacional: " + (Get-ComputerInfo).WindowsProductName,
-    "Versão do Windows: " + (Get-ComputerInfo).WindowsVersion,
-    "Processador: " + (Get-CimInstance Win32_Processor).Name,
-    "Memória RAM: " + [math]::Round((Get-ComputerInfo).CsTotalPhysicalMemory / 1GB, 2) + " GB"
+    "",
+    "╔═══════════════════════════════════════", 
+    "╠══════ Informações do Computador ══════", 
+    "╠═ Nome do Host: " + [System.Environment]::MachineName,
+    "╠═ Sistema Operacional: " + (Get-ComputerInfo).WindowsProductName,
+    "╠═ Versão do Windows: " + (Get-ComputerInfo).WindowsVersion,
+    "╠═ Processador: " + (Get-CimInstance Win32_Processor).Name,
+    "╠═ Memória RAM: " + [math]::Round((Get-ComputerInfo).CsTotalPhysicalMemory / 1GB, 2) + " GB",
+    "╚═══════════════════════════════════════",
+
   )
   $colors = @("VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "VerdeClaro", "AzulClaro", "AmareloClaro", "AmareloClaro", "VermelhoClaro", "Branco", "CianoClaro", "CianoClaro", "CianoClaro", "CianoClaro", "CianoClaro")
     
